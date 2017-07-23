97c0cca9ca30f3589ee8bbb997e33624073ded24
Mathieu MISDRAHI <mmisdrah@e3r11p7.42.fr>
2017-07-23 13:21:21 +0200

    v01

--- main.c
+++ main.c
@@ -6,22 +6,47 @@
 /*   By: mmisdrah <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/22 22:03:04 by mmisdrah          #+#    #+#             */
-/*   Updated: 2017/07/23 00:31:19 by mmisdrah         ###   ########.fr       */
+/*   Updated: 2017/07/23 13:20:37 by mmisdrah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define LENGTH 100
 #include "tools.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+int		affichage(int i, int tmp, int tmp2)
+{
+	int t;
+
+	t = 0;
+	if (t == 1)
+		ft_putstr(" || ");
+	ft_putstr("[colle-0");
+	ft_putchar(i + '0');
+	ft_putstr("] [");
+	ft_putchar((tmp + '0'));
+	ft_putstr("] [");
+	ft_putchar((tmp2 + '0'));
+	ft_putstr("]");
+	t = 1;
+	return (0);
+}
+
 int		main(void)
 {
-	char val[LENGTH] = {0};
+	int lenght = 0;
+	char *val;
 	int ret = 0, number = 0;
 	int tmp = 0, tmp2 = 0;
-	ret = read(0, val, LENGTH-1);
+
+	while (val[ret - 1] != '\0')
+	{
+		lenght += 10;
+		ret = read(0, val, lenght);
+	}
+	val = malloc(sizeof(char) * ret);
+	ret = read(0, val, lenght - 1);
 	val[ret] = '\0';
 	printf("%s\n", val);
 
@@ -37,21 +62,30 @@ int		main(void)
 			tmp2++;
 		ret++;
 	}
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = malloc(sizeof(char) * ((tmp * tmp2) + tmp2 + 1));
+
 	str = rush00(tmp, tmp2);
 	if (ft_strcmp(str, val) == 0)
-		ft_putstr("[colle-00] [");
-		ft_putchar((tmp + '0'));
-		ft_putstr("] [");
-		ft_putchar((tmp2 + '0'));
-		ft_putstr("]");
-
+		affichage(i, tmp, tmp2);
+	i++;
 	str = rush01(tmp, tmp2);
 	if (ft_strcmp(str, val) == 0)
-		if ()
-		ft_putstr("[colle-01] [");
-		ft_putchar((tmp + '0'));
-		ft_putstr("] [");
-		ft_putchar((tmp2 + '0'));
-		ft_putstr("]");
-
+		affichage(i, tmp, tmp2);
+	i++;
+	str = rush02(tmp, tmp2);
+	if (ft_strcmp(str, val) == 0)
+		affichage(i, tmp, tmp2);
+	i++;
+	str = rush03(tmp, tmp2);
+	if (ft_strcmp(str, val) == 0)
+		affichage(i, tmp, tmp2);
+	i++;
+	str = rush04(tmp, tmp2);
+	if (ft_strcmp(str, val) == 0)
+		affichage(i, tmp, tmp2);
+	return (0);
 }
